b03e9c5125cecf9610382135236f175771f08f87
vbpaladiya02 <vbpaladiya02@gmail.com>
2025-05-25 14:37:33 +0530

    card added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Product from './components/Products/Products'
+
+const App = () => {
+    return (
+        <div>
+           <Product />
+        </div>
+    )
+}
+
+export default App
